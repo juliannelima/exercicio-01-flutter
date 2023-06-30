f949d88027afa9931411cb15e0a8e9f216ea6bc9
Julianne <juliannelicon@mpto.mp.br>
2023-06-30 17:46:57 -0300

    .

--- lib/main.dart
+++ lib/main.dart
@@ -12,103 +12,163 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: 'Exercício 01',
       theme: ThemeData(
-          colorScheme: ColorScheme.fromSeed(seedColor: const Color(0xff009873)),
+          // colorScheme: ColorScheme.fromSeed(seedColor: const Color(0xff009873)),
           // useMaterial3: true,
           ),
-      home: Container(
-        color: const Color(0xFFf2fbf8),
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.spaceBetween,
-          children: [
-            Stack(
-              children: [
-                Container(
-                  width: 100,
-                  height: 100,
-                  color: Colors.red,
-                ),
-              ],
-            ),
-            Stack(
-              children: [
-                Row(
-                  mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                  children: [
-                    Column(children: [
-                      const Padding(
-                        padding: EdgeInsets.only(bottom: 16.0),
-                        child: Text(
-                          'Titulo',
-                          style: TextStyle(color: Colors.black),
-                          textAlign: TextAlign.left,
-                        ),
-                      ),
-                      const Padding(
-                        padding: EdgeInsets.only(bottom: 16.0),
-                        child: Text(
-                          'Lorem Ipsum is simply dummy text of the printing and typesetting industry.',
-                          style: TextStyle(color: Colors.black, fontSize: 16),
-                          textAlign: TextAlign.left,
-                        ),
-                      ),
-                      const SizedBox(height: 30),
-                      ElevatedButton(
-                        onPressed: () {},
-                        child: const Text('view open positions'),
-                        // style: ButtonStyle(backgroundColor: Color(0xff009872),),
-                      ),
-                    ]),
-                    Image.asset(
-                      'assets/images/imagem.png',
-                      fit: BoxFit.cover,
-                    ),
-                  ],
-                ),
-              ],
-            ),
-            Padding(
-              padding: const EdgeInsets.only(bottom: 10.0),
-              child: Row(
-                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                crossAxisAlignment: CrossAxisAlignment.end,
+      home: Scaffold(
+        body: Padding(
+          padding: const EdgeInsets.all(32.0),
+          child: Container(
+            // color: Colors.green,
+            color: const Color(0xFFf2fbf8),
+            child: const Padding(
+              padding: EdgeInsets.symmetric(horizontal: 100, vertical: 16),
+              child: Column(
                 children: [
-                  Container(
-                    width: 120,
-                    height: 120,
-                    color: Colors.amber,
-                    child: const Column(
-                      children: [
-                        Icon(Icons.headset),
-                        Text(
-                          'Card 1',
-                          style: TextStyle(fontSize: 16, color: Colors.black),
-                          textAlign: TextAlign.center,
-                        ),
-                      ],
-                    ),
+                  Header(),
+                  SizedBox(
+                    height: 24,
                   ),
-                  Container(
-                    width: 100,
-                    height: 100,
-                    color: Colors.white,
-                  ),
-                  Container(
-                    width: 100,
-                    height: 100,
-                    color: Colors.white,
-                  ),
-                  Container(
-                    width: 100,
-                    height: 100,
-                    color: Colors.white,
-                  ),
-                  Container(
-                    width: 100,
-                    height: 100,
-                    color: Colors.white,
-                  ),
+                  Main(),
                 ],
               ),
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
+
+class Header extends StatelessWidget {
+  const Header({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Row(
+      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+      children: [
+        Text('logo'),
+        Text('menu'),
+        Text('profile'),
+      ],
+    );
+  }
+}
+
+class Main extends StatelessWidget {
+  const Main({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Column(
+      children: [
+        ContentWidget(),
+        SizedBox(height: 32),
+        ListCardsWidget(),
+      ],
+    );
+  }
+}
+
+class ContentWidget extends StatelessWidget {
+  const ContentWidget({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+      children: [
+        const Column(
+          children: [
+            Text('titulo'),
+            Text('paragrafo'),
+            // ElevatedButton(
+            //   onPressed: () {},
+            //   child: Text('view'),
+            // )
+          ],
+        ),
+        Image.asset(
+          'assets/images/imagem.png',
+          fit: BoxFit.cover,
+        ),
+      ],
+    );
+  }
+}
+
+class ListCardsWidget extends StatelessWidget {
+  const ListCardsWidget({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Row(
+      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+      children: [
+        CardWidget(
+          texto: 'We re all on the same team',
+          icon: Icons.groups_2_outlined,
+          cor: Color(0xFFf4d076),
+        ),
+        CardWidget(
+          texto: 'A balanced life is a healthy life',
+          icon: Icons.favorite_border_outlined,
+          cor: Color(0xffca7270),
+        ),
+        CardWidget(
+          texto: '100% human',
+          icon: Icons.accessibility_new_outlined,
+          cor: Colors.deepPurple,
+        ),
+        CardWidget(
+          texto: 'Start with service',
+          icon: Icons.support_agent_outlined,
+          cor: Colors.green,
+        ),
+        CardWidget(
+          texto: 'Do the right thing',
+          icon: Icons.thumb_up_off_alt_outlined,
+          cor: Colors.blue,
+        ),
+      ],
+    );
+  }
+}
+
+class CardWidget extends StatelessWidget {
+  final IconData icon;
+  final String texto;
+  final Color cor;
+
+  const CardWidget({
+    super.key,
+    required this.icon,
+    required this.texto,
+    required this.cor,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Card(
+      elevation: 2,
+      margin: EdgeInsets.zero,
+      child: SizedBox(
+        width: 150,
+        height: 120,
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Icon(
+              icon,
+              size: 32,
+              color: cor,
+            ),
+            const SizedBox(height: 8),
+            Text(
+              texto,
+              style: const TextStyle(fontWeight: FontWeight.bold),
+              textAlign: TextAlign.center,
             ),
           ],
         ),
